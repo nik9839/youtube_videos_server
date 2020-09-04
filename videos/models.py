79e4ee817ccaf1f2954abca2a60b426331fdd87b
Nikhil Gupta <nikhilgupta709@gmail.com>
2020-09-04 17:24:17 +0530

    get videos list api added

--- videos/models.py
+++ videos/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.postgres.fields import JSONField
 from django.utils.translation import ugettext_lazy as _
 
-class GeneralApp(models.Model):
+class Videos(models.Model):
     title = models.CharField(_('video title'), max_length=100, blank=True)
     description = models.TextField(_('Video Description'), blank=True, null=True)
     published_at = models.DateTimeField(_('published at'), default=timezone.now)
--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from videos.models import Videos
+from videos.serializers import VideoListSerializer
 
-# Create your views here.
+
+class VideosViewSet(viewsets.ReadOnlyModelViewSet):
+
+    model = Videos
+    serializer_class = VideoListSerializer
+
+    def get_queryset(self):
+        """
+            RETURN Videos queryset
+        """
+
+        queryset = self.model.objects.order_by("id") 
+        
+        return queryset
+    
