f208d443c1904cade31f3b06ae4647345f6986c5
Nikhil Gupta <nikhilgupta709@gmail.com>
2020-09-05 01:21:36 +0530

    Youtube api key model added

--- videos/admin.py
+++ videos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Videos
+from .models import Videos, YoutubeKeys
 
 # Register your models here.
 
@@ -8,4 +8,13 @@ class VideosAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'published_at')
     list_display_links = ('id', 'title', )
     date_hierarchy = 'published_at'
+    show_full_result_count = False
+
+
+
+@admin.register(YoutubeKeys)
+class YoutubeKeysAdmin(admin.ModelAdmin):
+    list_display = ('id', 'key', 'added_at')
+    list_display_links = ('id', 'key', )
+    date_hierarchy = 'added_at'
     show_full_result_count = False
--- videos/models.py
+++ videos/models.py
@@ -6,5 +6,11 @@ from django.utils.translation import ugettext_lazy as _
 class Videos(models.Model):
     title = models.CharField(_('video title'), max_length=100, blank=True)
     description = models.TextField(_('Video Description'), blank=True, null=True)
-    published_at = models.DateTimeField(_('published at'), default=timezone.now)
+    published_at = models.DateTimeField(_('published at'), default=timezone.now, db_index=True)
     thumbnails = JSONField(null=True, blank=True)
+
+
+
+class YoutubeKeys(models.Model):
+    key = models.CharField(_("api key"), max_length=50)
+    added_at = models.DateTimeField(_('created at'), auto_now_add=True)
