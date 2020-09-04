5ebcddd5cf3cd033efd054067e89582a7ed9cce8
Nikhil Gupta <nikhilgupta709@gmail.com>
2020-09-04 17:34:21 +0530

    admin view added for video app

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Videos
 
 # Register your models here.
+
+@admin.register(Videos)
+class VideosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'published_at')
+    list_display_links = ('id', 'title', )
+    date_hierarchy = 'published_at'
+    show_full_result_count = False
